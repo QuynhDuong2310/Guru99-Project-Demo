b5757a0d68e8a1d189f441578b38cd1fd152e118
QuynhDuong2310 <Quynhxoansh95@gmail.com>
2024-03-01 21:15:32 +0700

    Level 01 Do not repeat yourself.

--- actions/commons/GlobalConstants.java
+++ actions/commons/GlobalConstants.java
@@ -1,4 +1,19 @@
 package commons;
 
 public class GlobalConstants {
+    //Chứa dữ liệu dùng cho toàn bộ hệ thống
+    // Link server
+    // Credentials: Username/Password
+    // Tên Database/Username/Password cho từng server
+    // Biến timeout long/short
+    // ố lần retry la test failed
+    // Đường dẫn tương đối đến thư mục download/file data(xml/ excel/ json/...)/ file upload
+
+
+    public static final String DEV_SERVER = "";
+    public static final String TEST_SERVER = "";
+    public static final String STG_SERVER = "";
+    public static final long  SHORT_TIMEOUT = 5;
+    public static final long  LONG_TIMEOUT = 30;
+
 }
